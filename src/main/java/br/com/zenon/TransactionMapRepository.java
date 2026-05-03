136fe562c83efeafb1bc67ce483dfe5866d56844
Marcos Coscos <marcosafcs@hotmail.com>
2026-05-03 16:23:05 -0300

    Crated TransactionRepositories to benchmark difference between List and Map

---
+++ src/main/java/br/com/zenon/TransactionMapRepository.java
@@ -0,0 +1,29 @@
+package br.com.zenon;
+
+import br.com.zenon.fraud.model.Transaction;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class TransactionMapRepository implements TransactionRepository {
+
+    private final Map<String, Transaction> transactions;
+
+    public TransactionMapRepository(List<Transaction> transactions) {
+        if (transactions == null || transactions.isEmpty()) {
+            this.transactions = Map.of();
+        } else {
+            this.transactions = transactions.stream().collect(Collectors.toMap(Transaction::nameOrig, t -> t));
+        }
+    }
+
+    public Optional<Transaction> getByNameOrig(String nameOrig) {
+        long startTime = System.nanoTime();
+        Transaction transaction = transactions.get(nameOrig);
+        long endTime = System.nanoTime();
+        IO.println("Time to find transaction by nameOrig=[" + nameOrig + "] using MAP: " + (endTime - startTime));
+        return Optional.ofNullable(transaction);
+    }
+}
---
+++ src/main/java/br/com/zenon/TransactionRepository.java
@@ -0,0 +1,10 @@
+package br.com.zenon;
+
+import br.com.zenon.fraud.model.Transaction;
+
+import java.util.Optional;
+
+public interface TransactionRepository {
+
+    Optional<Transaction> getByNameOrig(String nameOrig);
+}
