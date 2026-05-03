f5b08b1b7e93883bef25630fa2da473e1b48d55f
Coscos007 <140120620+Coscos007@users.noreply.github.com>
2026-05-03 00:18:16 -0300

    Added validations for Transaction object creation

--- src/main/java/br/com/zenon/Main.java
+++ src/main/java/br/com/zenon/Main.java
@@ -1,9 +1,17 @@
 package br.com.zenon;
 
+import br.com.zenon.fraud.Transaction;
+
+import java.util.List;
+
 public class Main {
     static void main() {
         TransactionIngestor ingestor = new TransactionIngestor();
-        var transactions = ingestor.ingest("./data/PS_20174392719_1491204439457_log.csv", 10);
-        transactions.forEach(IO::println);
+
+        //List<Transaction> transactions = ingestor.ingest("./data/PS_20174392719_1491204439457_log.csv", 10d);
+        //transactions.forEach(IO::println);
+
+        List<Transaction> transactionsError = ingestor.ingest("./data/paysim_with_bad_data.csv", null);
+        transactionsError.forEach(IO::println);
     }
 }
--- src/main/java/br/com/zenon/TransactionIngestor.java
+++ src/main/java/br/com/zenon/TransactionIngestor.java
@@ -1,13 +1,11 @@
 package br.com.zenon;
 
 import br.com.zenon.fraud.Transaction;
-import br.com.zenon.fraud.TransactionType;
 import br.com.zenon.utils.IngestorUtils;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -16,24 +14,30 @@ import java.util.List;
 
 public class TransactionIngestor {
 
-    public List<Transaction> ingest(String filePath, double amountThreshold) {
+    public List<Transaction> ingest(String filePath, Double amountThreshold) {
+        if (amountThreshold == null) {
+            amountThreshold = Double.MAX_VALUE;
+        }
         Path path = Paths.get(filePath);
         try (BufferedReader br = new BufferedReader(new FileReader(path.toFile()))) {
             br.readLine(); // skip csv headers
 
             var transactions = new ArrayList<Transaction>();
+            int errorCount = 0;
             String line;
             while ((line = br.readLine()) != null && transactions.size() < amountThreshold) {
                 String[] columns = line.split(",");
-                var transaction = new Transaction(Integer.parseInt(columns[0]), TransactionType.valueOf(columns[1]), new BigDecimal(columns[2]),
-                        columns[3], new BigDecimal(columns[4]), new BigDecimal(columns[5]),
-                        columns[6], new BigDecimal(columns[7]), new BigDecimal(columns[8]),
-                        IngestorUtils.parseBoolean(columns[9]), IngestorUtils.parseBoolean(columns[10]));
-                transactions.add(transaction);
+                try {
+                    transactions.add(IngestorUtils.buildTransaction(columns));
+                } catch (IllegalArgumentException e) {
+                    System.err.println("Erro: " + line + " | " + e);
+                    errorCount++;
+                }
             }
+            IO.println(errorCount + " lines with errors");
             return transactions;
         } catch (IOException e) {
-            IO.println("ERROR: Failed to read file [" + filePath + "] with the following error: " + e.getMessage());
+            System.err.println("ERROR: Failed to read file [" + filePath + "] with error: " + e.getMessage());
             return Collections.emptyList();
         }
     }
